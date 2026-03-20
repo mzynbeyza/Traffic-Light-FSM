b7fc20996f43e70fa648269e771a9975ebc49aea
mzynbeyza <beyzatutk02@gmail.com>
2026-03-20 20:02:14 +0300

    Update tb_Trafficlights_FSM.sv

--- tb_Trafficlights_FSM.sv
+++ tb_Trafficlights_FSM.sv
@@ -27,23 +27,23 @@ module tb_Trafficlights_FSM();
         // Initialize Inputs
         clk = 0;
         reset = 1;
-        TAORB = 1; // Start with traffic on Street A [cite: 21]
+        TAORB = 1; // Start with traffic on Street A 
 
         // Wait 20ns for global reset
         #20;
         reset = 0;
 
-        // --- Test Scenario 1: Transition from S0 to S1 ---
+        // Test Scenario 1: Transition from S0 to S1
         #20;
-        TAORB = 0; // No more traffic at A, transition to S1 [cite: 22]
+        TAORB = 0; // No more traffic at A, transition to S1
         
         // Stay in S1 (Yellow) for 5 clock cycles 
         repeat (6) @(posedge clk); 
 
-        // --- Test Scenario 2: Transition from S2 to S3 ---
-        TAORB = 1; // Traffic returns to A, transition to S3 [cite: 26]
+        // Test Scenario 2: Transition from S2 to S3
+        TAORB = 1; // Traffic returns to A, transition to S3 
         
-        // Stay in S3 (Yellow) for 5 clock cycles [cite: 29]
+        // Stay in S3 (Yellow) for 5 clock cycles
         repeat (6) @(posedge clk);
 
         #50;
@@ -57,3 +57,4 @@ module tb_Trafficlights_FSM();
     end
 
 endmodule
+
